387a09a071a913a0b974e1af0597f4a306175a35
Noura Zekry <48691061+nourazekry@users.noreply.github.com>
2023-07-08 18:52:59 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,62 @@
-
+// https://www.youtube.com/watch?v=W8qA0Q0sBoQ&t=581s
 let cam;
 
 function setup() {
-  cam = createCapture(VIDEO, function(){
-    createCanvas(cam.width, cam.height); 
-    cam.hide();
-  });
+  createCanvas(400, 400, WEBGL);
+  cam = createCapture();
+  cam.hide();
 }
 
 function draw() {
   background(220);
-  image(cam, 0, 0);
-  push();
-  scale(-1, 1);
-  // Copy left half of the camera image
-  // To the right half of the canvas
-  copy(cam, 0, 0, width/2, height, -width, 0, width/2, height);
-  // rect(-width, 0, width/2, height);
-  // ellipse(-width, 0, 50, 50);
-  tint(255, 128*sin(frameCount*0.01) + 127)  
-  image(cam, -width, 0);
-  pop();
+  noStroke();
+
+  push()
+  rotateZ(QUARTER_PI * 8)
+  texture(cam)
+  plane(400, 400)
+  pop()
+
+  push()
+  rotateZ(QUARTER_PI * 7.5)
+  texture(cam)
+  plane(350, 350)
+  pop()
+
+  push()
+  rotateZ(QUARTER_PI * 7)
+  texture(cam)
+  plane(300, 300)
+  pop()
+
+  push()
+  rotateZ(QUARTER_PI * 6.5)
+  texture(cam)
+  plane(250, 250)
+  pop()
+
+  push()
+  rotateZ(QUARTER_PI * 6)
+  texture(cam)
+  plane(200, 200)
+  pop()
+
+  push()
+  rotateZ(QUARTER_PI * 5.5)
+  texture(cam)
+  plane(150, 150)
+  pop()
+
+  push()
+  rotateZ(QUARTER_PI * 5)
+  texture(cam)
+  plane(100, 100)
+  pop()
+
+
+  push()
+  rotateZ(QUARTER_PI * 4)
+  texture(cam)
+  plane(30, 30)
+  pop()
 }
